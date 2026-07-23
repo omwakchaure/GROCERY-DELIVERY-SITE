d0c2f4a714837f6bfa79615e66346b156bb6fb20
omwakchaure <omwakchaure9823@gmail.com>
2026-07-23 23:39:12 +0530

    FEAT : forms page login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,115 @@
-
+import { useState } from "react";
+import { heroSectionData } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { BikeIcon, UserIcon } from "lucide-react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isLoginState, setIsLoginState] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default Login
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => (window.location.href = "/"), 1000);
+  };
+
+  return (
+    <div className="min-h-screen flex">
+      {/* left side */}
+
+      <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center jsutify-center">
+        <img
+          src={heroSectionData.hero_image}
+          alt=""
+          className="absolute inset-0 object-cover h-full bg-center opacity-10"
+        />
+        <div className="relative text-center px-44">
+          <h2 className="text-white font-semibold max-w-sm mb-4 text-4xl">
+            {" "}
+            Welcome back to Instacart
+          </h2>
+          <p className="text-white/60 font-serif text-xl max-w-sm mx-auto ">
+            Fresh groceries and organic produce, delivered to your doorstep
+          </p>
+        </div>
+      </div>
+
+      {/* right side */}
+
+      {/* Right Side */}
+      <div className="flex-1 flex-center px-4 py-12 bg-app-cream">
+        <div className="w-full max-w-md">
+          {/* form header message */}
+          <div className="text-center mb-8">
+            <Link to="/" className="inline-flex items-center gap-2 mb-6">
+              <BikeIcon className="size-8 text-app-green" />
+              <span className="text-2xl font-semibold text-app-green">
+                Instacart
+              </span>
+            </Link>
+
+            <h1 className="text-2xl font-semibold text-app-green mb-2">
+              {isLoginState
+                ? "Sign in to your account"
+                : "Sign up for an account"}
+            </h1>
+
+            <p className="text-sm text-app-text-light">
+              {isLoginState
+                ? "Don't have an account?"
+                : "Already have an account?"}
+
+              <button
+                onClick={() => setIsLoginState(!isLoginState)}
+                className="text-orange-500 ml-1 font-semibold hover:text-orange-600 transition-colors"
+              >
+                {isLoginState ? "Create one" : "Sign in"}
+              </button>
+            </p>
+          </div>
+
+          {/* Login / Register Form */}
+          <form onSubmit={handleSubmit} className="space-y-5">
+            {!isLoginState && (
+              <label className="text-sm flex flex-col gap-1">
+                Name
+                <div className="relative">
+                  <UserIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    placeholder="Your name"
+                    className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                  />
+                </div>
+              </label>
+            )}
+            <label className="text-sm flex flex-col gap-1">
+              Email Address
+              <div className="relative">
+                <UserIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="you@example.com"
+                  className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                />
+              </div>
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
