35622a2ab028e90aaae10bc6b67f305e771168aa
omwakchaure <omwakchaure9823@gmail.com>
2026-08-05 23:07:02 +0530

    FEAT : new page products started

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,38 @@
-
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import type { Product } from "../types";
+import { dummyProducts } from "../assets/assets";
 
 const Products = () => {
-  return (
-    <div>
-      <p>products</p>
-    </div>
-  )
-}
+  const [searchParams, setSearchParams] = useSearchParams();
 
-export default Products
+  const [products, setProducts] = useState<Product[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+
+  const category = searchParams.get("category") || "";
+  const organic = searchParams.get("organic") || "";
+  const sort = searchParams.get("sort") || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const minPrice = searchParams.get("minPrice") || "";
+  const maxPrice = searchParams.get("maxPrice") || "";
+
+  const fetchProducts = async () => {
+    setLoading(true);
+
+    setProducts(
+      dummyProducts.filter(
+        (p) => p.category === category || category === ""
+      )
+    );
+
+    setLoading(false);
+  };
+
+  return (
+    <div>Products</div>
+  );
+};
+
+export default Products;
