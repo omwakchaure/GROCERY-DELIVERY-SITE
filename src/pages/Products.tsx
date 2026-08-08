e739422a0844e07ba7cfcbcb2f8623b0d22a2239
omwakchaure <omwakchaure9823@gmail.com>
2026-08-08 15:12:22 +0530

    FEAT : filter panel component implementation

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,7 +4,8 @@ import type { Product } from "../types";
 import { categoriesData, dummyProducts } from "../assets/assets";
 import ProductCard from "../components/ProductCard";
 import { ChevronDown, Home, SlidersHorizontal } from "lucide-react";
-import Loading from "../components/loading";
+import Loading from "../components/Loading";
+import FilterPanel from "../components/FilterPanel";
 
 const Products = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -78,7 +79,16 @@ const Products = () => {
           {/* Sidebar - Desktop */}
           <aside className="hidden lg:block w-64 shrink-0">
             <div className="bg-white rounded-2xl p-4 sticky top-24">
-              <p>Filter</p>
+              <FilterPanel
+                categories={categoriesData}
+                category={category}
+                organic={organic}
+                minPrice={minPrice}
+                maxPrice={maxPrice}
+                updateFilter={updateFilter}
+                clearFilters={clearFilters}
+                hasFilters={hasFilters}
+              />
             </div>
           </aside>
 
